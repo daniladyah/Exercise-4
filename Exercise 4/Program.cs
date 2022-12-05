7dac2e8168eac257a0ce89711943060b85661dcc
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2022-12-05 15:48:30 +0700

    Add project files.

---
+++ Exercise 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4
+{
+    class Node
+    {
+        public int info;
+        public node next;
+        public node(int i, node n)
+        {
+
+        }
+    }
+    
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
